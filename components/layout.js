ae03768ef614d4d2b323f4a6ad72722d53384d78
Katie Maison <kmm565@york.ac.uk>
2023-08-13 22:11:12 +0100

    Change padding

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({children}){
             </Head>
             <div className="w-full bg-[#003049] m-0 flex flex-row justify-between">
 
-                <div id="logo-wrapper " className="pr-2 pl-32 w-auto h-16 flex p-4">
+                <div id="logo-wrapper" className="pr-2 ml-4 w-auto h-16 flex p-4">
                     <svg class="abstract-logo logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 51" preserveAspectRatio="xMinYMid slice">
                         <g class="abstract-logo-mark" fill="#fff">
                             <path d="M25.41 0C5.08 0 0 5.08 0 25.41s5.08 25.41 25.41 25.41 25.41-5.08 25.41-25.41S45.74 0 25.41 0zm-4.17 39.31A9.73 9.73 0 1 1 31 29.58a9.75 9.75 0 0 1-9.76 9.73zm17.67-.4h-4.77V16.67H11.91v-4.76h27z"></path>
