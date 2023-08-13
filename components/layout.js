87c6a631998065af546c399af9ea78361c20c4e5
Katie Maison <kmm565@york.ac.uk>
2023-08-13 22:54:38 +0100

    Oops

--- components/layout.js
+++ components/layout.js
@@ -41,7 +41,7 @@ export default function Layout({children}){
                     <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 border border-white ">Submit a request</button>
                     <button type="button" className="text-white bg-[#669BBC] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2 focus:outline-none border border-[#669BBC]">Sign in</button>
                 </div>
-                <div className = "m-auto mr-1">
+                <div>
                     <button onClick={handleToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg bg-none md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
