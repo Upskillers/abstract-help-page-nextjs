ec618e2da2153bcf7ff9a936db16a59eeeb2ad0d
Ben Jackson <jackson.8en@gmail.com>
2023-08-18 19:30:56 +0100

    finish set up of layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,35 +4,35 @@ import Link from 'next/link'
 export const siteTitle = 'Upskill: Abstract Help Center'
 
 export default function Layout({ children, home }) {
-    return (
-            <div className='container bg-[#669BBC]
+  return (
+    <div className='bg-[#669BBC]
         min-h-[100vh] px-0 py-[0.5rem] flex flex-col justify-center items-center'>
-            <Head>
-                <title>{siteTitle}</title>
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="og:title" content={siteTitle} />
-            </Head>
-                <h1>I'm a missing navbar, hear me ROAR!</h1>
-                <main className='py-[5rem] flex grow flex-col flex-wrap items-center justify-center'>
-                    {children}
-                </main>
-                {!home && (
-                    <div>
-                        <Link href='/'>Back to home</Link>
-                    </div>
-                )}
-                <footer>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Powered by{' '}
-                        <img src="/vercel.svg" alt="Vercel" className='' />
-                    </a>
-                </footer>
-            </div>
-    )
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <h1>I'm a missing navbar, hear me ROAR!</h1>
+      <main className='py-[5rem] flex grow flex-col flex-wrap items-center justify-center'>
+        {children}
+      </main>
+      {!home && (
+        <div>
+          <Link href='/'>Back to home</Link>
+        </div>
+      )}
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className='' />
+        </a>
+      </footer>
+    </div>
+  )
 }
 {/* <style jsx>{`
         footer {
