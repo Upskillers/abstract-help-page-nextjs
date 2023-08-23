4dfe49ac3918ba0fd0910b66b29d6c6bae325815
Katie Maison <kmm565@york.ac.uk>
2023-08-23 21:18:33 +0100

    Add darkmode button

--- components/navbar.js
+++ components/navbar.js
@@ -6,23 +6,35 @@ export default function Navbar() {
     const handleToggle = () => {
         setVisible((current) => !current);
     };
- 
+
     const [theme, setTheme] = useState(true);
     // for dark mode
     useEffect(() => {
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
         const theme = JSON.parse(localStorage.getItem('theme'));
         // if not theme and user OS is dark mode
         if ((!theme && 
-        window.matchMedia('(prefers-color-scheme: dark)').matches) ||           theme === 'dark') {
-        setTheme('dark');
-        document.documentElement.classList.add('dark');
+            window.matchMedia('(prefers-color-scheme: dark)').matches) ||           theme === 'dark') {
+            setTheme('dark');
+            document.documentElement.classList.add('dark');
+            themeToggleLightIcon.classList.remove('hidden');
         } else {
-        setTheme('light');
-        document.documentElement.classList.remove('dark');
+            setTheme('light');
+            document.documentElement.classList.remove('dark');
+            themeToggleDarkIcon.classList.remove('hidden');
+
         }
     }, [theme]);
 
     function handleDarkToggle() {
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+        // toggle icons inside button
+        themeToggleDarkIcon.classList.toggle('hidden');
+        themeToggleLightIcon.classList.toggle('hidden');
+
+
         if (theme === "light") {
             // when theme is changed, set the localStorage
             localStorage.setItem("theme", JSON.stringify("dark"));
@@ -41,8 +53,7 @@ export default function Navbar() {
     
     return (
         <div className="w-full">
-        <div className="{{ dark ? 'dark' : '' }} w-full bg-[#003049] m-0 flex flex-row justify-between">
-        <button onClick={handleDarkToggle}>Dark</button>
+        <div className="w-full bg-[#003049] m-0 flex flex-row justify-between">
             <div id="logo-wrapper" className="pr-2 m-1 md:ml-4 w-auto h-16 flex p-4">
                 <svg className="abstract-logo logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 51" preserveAspectRatio="xMinYMid slice">
                     <g className="abstract-logo-mark" fill="#fff">
@@ -64,6 +75,11 @@ export default function Navbar() {
                 <a className="text-white m-auto ml-0" title="Home" href="#">Help Center</a>
             </div>
             <div className="m-auto mr-4 hidden md:block">
+                <button onClick={handleDarkToggle} className = "m-auto mr-4 align-middle">
+                    <svg id="theme-toggle-dark-icon" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                    <svg id="theme-toggle-light-icon" className="hidden w-5 h-5 fill-white " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                </button>
+
                 <button type="button" className="text-white bg-gray-800 hover:bg-gray-900
                                                  focus:outline-none focus:ring-4 focus:ring-gray-300
                                                  font-medium rounded-lg text-sm px-5 py-2.5 border
