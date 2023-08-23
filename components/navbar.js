7876e1a85e84c3c265ce57225ac3a284bcecc27e
Katie Maison <kmm565@york.ac.uk>
2023-08-23 21:44:32 +0100

    Remove mobile button

--- components/navbar.js
+++ components/navbar.js
@@ -12,8 +12,7 @@ export default function Navbar() {
     useEffect(() => {
         var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
         var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-        var themeToggleDarkIcon2 = document.getElementById('theme-toggle-dark-icon2');
-        var themeToggleLightIcon2 = document.getElementById('theme-toggle-light-icon2');
+
         const theme = JSON.parse(localStorage.getItem('theme'));
         // if not theme and user OS is dark mode
         if ((!theme && 
@@ -21,13 +20,11 @@ export default function Navbar() {
             setTheme('dark');
             document.documentElement.classList.add('dark');
             themeToggleLightIcon.classList.remove('hidden');
-            themeToggleLightIcon2.classList.remove('hidden');
 
         } else {
             setTheme('light');
             document.documentElement.classList.remove('dark');
             themeToggleDarkIcon.classList.remove('hidden');
-            themeToggleDarkIcon2.classList.remove('hidden');
 
 
         }
@@ -36,13 +33,9 @@ export default function Navbar() {
     function handleDarkToggle() {
         var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
         var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-        var themeToggleDarkIcon2 = document.getElementById('theme-toggle-dark-icon2');
-        var themeToggleLightIcon2 = document.getElementById('theme-toggle-light-icon2');
-        // toggle icons inside button
+
         themeToggleDarkIcon.classList.toggle('hidden');
         themeToggleLightIcon.classList.toggle('hidden');
-        themeToggleDarkIcon2.classList.toggle('hidden');
-        themeToggleLightIcon2.classList.toggle('hidden');
 
 
         if (theme === "light") {
@@ -84,11 +77,11 @@ export default function Navbar() {
                 <div className="inline-block w-0.5 self-stretch my-0.5 mx-2 bg-[#C1121F]"></div>
                 <a className="text-white m-auto ml-0" title="Home" href="#">Help Center</a>
             </div>
-            <div className="m-auto mr-4 hidden md:block">
-                <button onClick={handleDarkToggle} className = "m-auto mr-4 align-middle">
+            <button onClick={handleDarkToggle} className = "m-auto mr-4 align-middle">
                     <svg id="theme-toggle-dark-icon" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
                     <svg id="theme-toggle-light-icon" className="hidden w-5 h-5 fill-white " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                </button>
+            </button>
+            <div className="m-auto ml-1 mr-4 hidden md:block">
 
                 <button type="button" className="text-white bg-gray-800 hover:bg-gray-900
                                                  focus:outline-none focus:ring-4 focus:ring-gray-300
@@ -100,10 +93,7 @@ export default function Navbar() {
                                                  focus:outline-none border border-[#669BBC]">Sign in</button>
             </div>
             <div className="m-auto mr-1 md:hidden">
-            <button onClick={handleDarkToggle} className = "m-auto mr-4 align-middle">
-                    <svg id="theme-toggle-dark-icon2" className="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-                    <svg id="theme-toggle-light-icon2" className="hidden w-5 h-5 fill-white " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                </button>
+
                 <button onClick={handleToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center
                                                                                                               p-2 w-10 h-10 justify-center
                                                                                                               text-sm text-gray-500 rounded-lg
